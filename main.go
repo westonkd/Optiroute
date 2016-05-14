58758afb97de3592608592eba2fbc3a28a2c63fe
westonkd <wdrans@gmail.com>
2016-05-14 15:41:41 -0600

    chagned listen port

--- main.go
+++ main.go
@@ -3,9 +3,16 @@ package main
 import (
 	_ "optiroute/routers"
 	"github.com/astaxie/beego"
+	"strconv"
+	"os"
 )
 
 func main() {
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+	if err == nil {
+		beego.BConfig.Listen.HTTPPort = port
+	}
+
 	beego.Run()
 }
 
