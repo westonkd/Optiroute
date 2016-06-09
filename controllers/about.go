d861f652d44492ecef9fceb1aaf4ca02e485f0f6
westonkd <wdrans@gmail.com>
2016-06-08 20:36:47 -0600

    increased

--- controllers/about.go
+++ controllers/about.go
@@ -52,7 +52,7 @@ func (c *AboutController) Post() {
 	response.Initial = ga.Pop.Chromosomes[rand.Intn(ga.PopSize - 1)]
 	response.InitialDistance = response.Initial.Distance()
 
-	for i := 0; i < 200; i++ {
+	for i := 0; i < 1000; i++ {
 		ga.Evolve()
 	}
 
